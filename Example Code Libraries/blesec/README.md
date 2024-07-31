3b7eccd2c4013c6e4fdb10734e810fb803ac5178
Pau Garcia <paugarcia32@gmail.com>
2024-08-01 00:32:22 +0200

    BLE Sec data tx
    Data communication between mobile and ESP32 using the LTK and IRK keys.
    Mobile sends String to ESP

---
+++ Example Code Libraries/blesec/README.md
@@ -0,0 +1,11 @@
+# BLE using LTK and IRK keys
+
+This arduino code, uses the [ArduinoBLE library](https://github.com/arduino-libraries/ArduinoBLE)
+
+It has been added a `onDataReceived`function, which recollects a String send by the mobile phone
+
+## Example
+
+## Reference
+
+- [ArduinoBLE example code](https://github.com/arduino-libraries/ArduinoBLE/blob/master/examples/Peripheral/EncryptedBatteryMonitor/EncryptedBatteryMonitor.ino)
--- Example Code Libraries/blesec/blesec.ino
+++ Example Code Libraries/blesec/blesec.ino
@@ -1,6 +1,6 @@
-#include <ArduinoBLE.h>
 
-#define CTRL_LED LED_BUILTIN
+
+#include <ArduinoBLE.h>
 
 // BLE Service
 BLEService testService("180F");
@@ -9,107 +9,151 @@ BLEService testService("180F");
 BLEStringCharacteristic stringCharacteristic("183E", BLERead | BLEWrite, 31);
 
 // BLE Secure Characteristic
-BLEUnsignedCharCharacteristic secureValue("2a3F", BLERead | BLEWrite | BLEEncryption);
+BLEUnsignedCharCharacteristic secureValue("2a3F",
+                                          BLERead | BLEWrite | BLEEncryption);
+
+// BLE Characteristic to receive data from mobile
+BLEStringCharacteristic dataCharacteristic("2A37", BLERead | BLEWrite, 31);
+
+void generateRandomKey(uint8_t *key, size_t length) {
+  for (size_t i = 0; i < length; i++) {
+    key[i] = random(0, 256);
+  }
+}
+
+void onDataReceived(BLEDevice central, BLECharacteristic characteristic) {
+  int length = characteristic.valueLength();
+  uint8_t value[length];
+  characteristic.readValue(value, length);
+
+  char buffer[length + 1]; // +1 para el terminador nulo
+  memcpy(buffer, value, length);
+  buffer[length] = '\0'; // agregar el terminador nulo
+
+  String receivedData = String(buffer);
+  Serial.print("Received data: ");
+  Serial.println(receivedData);
+}
 
 void setup() {
-  Serial.begin(115200);    // initialize serial communication
-  while (!Serial);
-
-  pinMode(CTRL_LED, OUTPUT); // initialize the built-in LED pin to indicate when a central is connected
+  Serial.begin(115200); // initialize serial communication
+  while (!Serial)
+    ;
 
   Serial.println("Serial connected");
-  
+
   // Callback function with confirmation code when new device is pairing.
-  BLE.setDisplayCode([](uint32_t confirmCode){
+  BLE.setDisplayCode([](uint32_t confirmCode) {
     Serial.println("New device pairing request.");
     Serial.print("Confirm code matches pairing device: ");
     char code[6];
     sprintf(code, "%06d", confirmCode);
     Serial.println(code);
   });
-  
+
   // Callback to allow accepting or rejecting pairing
-  BLE.setBinaryConfirmPairing([](){
+  BLE.setBinaryConfirmPairing([]() {
     Serial.print("Should we confirm pairing? ");
     delay(5000);
     Serial.println("yes");
     return true;
   });
 
-  // IRKs are keys that identify the true owner of a random mac address.
-  // Add IRKs of devices you are bonded with.
-  BLE.setGetIRKs([](uint8_t* nIRKs, uint8_t** BDaddrTypes, uint8_t*** BDAddrs, uint8_t*** IRKs){
-    // Set to number of devices
-    *nIRKs       = 2;
+  BLE.setGetIRKs([](uint8_t *nIRKs, uint8_t **BDaddrTypes, uint8_t ***BDAddrs,
+                    uint8_t ***IRKs) {
+    *nIRKs = 2;
 
-    *BDAddrs     = new uint8_t*[*nIRKs];
-    *IRKs        = new uint8_t*[*nIRKs];
+    *BDAddrs = new uint8_t *[*nIRKs];
+    *IRKs = new uint8_t *[*nIRKs];
     *BDaddrTypes = new uint8_t[*nIRKs];
 
-    // Set these to the mac and IRK for your bonded devices as printed in the serial console after bonding.
-    uint8_t device1Mac[6]    = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-    uint8_t device1IRK[16]   = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    uint8_t device1Mac[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    uint8_t device1IRK[16];
+    generateRandomKey(device1IRK, 16);
 
-    uint8_t device2Mac[6]    = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-    uint8_t device2IRK[16]   = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    uint8_t device2Mac[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    uint8_t device2IRK[16];
+    generateRandomKey(device2IRK, 16);
 
-
-    (*BDaddrTypes)[0] = 0; // Type 0 is for pubc address, type 1 is for static random
-    (*BDAddrs)[0] = new uint8_t[6]; 
-    (*IRKs)[0]    = new uint8_t[16];
-    memcpy((*IRKs)[0]   , device1IRK,16);
+    (*BDaddrTypes)[0] = 0;
+    (*BDAddrs)[0] = new uint8_t[6];
+    (*IRKs)[0] = new uint8_t[16];
+    memcpy((*IRKs)[0], device1IRK, 16);
     memcpy((*BDAddrs)[0], device1Mac, 6);
-
 
     (*BDaddrTypes)[1] = 0;
     (*BDAddrs)[1] = new uint8_t[6];
-    (*IRKs)[1]    = new uint8_t[16];
-    memcpy((*IRKs)[1]   , device2IRK,16);
+    (*IRKs)[1] = new uint8_t[16];
+    memcpy((*IRKs)[1], device2IRK, 16);
     memcpy((*BDAddrs)[1], device2Mac, 6);
-
 
     return 1;
   });
-  // The LTK is the secret key which is used to encrypt bluetooth traffic
-  BLE.setGetLTK([](uint8_t* address, uint8_t* LTK){
-    // address is input
+
+  BLE.setGetLTK([](uint8_t *address, uint8_t *LTK) {
     Serial.print("Received request for address: ");
-    btct.printBytes(address,6);
+    for (int i = 0; i < 6; i++) {
+      Serial.print(address[i], HEX);
+      if (i < 5)
+        Serial.print(":");
+    }
+    Serial.println();
 
-    // Set these to the MAC and LTK of your devices after bonding.
-    // uint8_t device1Mac[6]  = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-    // uint8_t device1LTK[16] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-    uint8_t device1Mac[6]  = {0x7c, 0x7f, 0x21, 0xf9, 0x65, 0xc5};
-    uint8_t device1LTK[16] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-    uint8_t device2Mac[6]  = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-    uint8_t device2LTK[16] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-    
+    uint8_t device1Mac[6] = {0x7c, 0x7f, 0x21, 0xf9, 0x65, 0xc5};
+    uint8_t device1LTK[16];
+    generateRandomKey(device1LTK, 16);
 
-    if(memcmp(device1Mac, address, 6) == 0) {
+    uint8_t device2Mac[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    uint8_t device2LTK[16];
+    generateRandomKey(device2LTK, 16);
+
+    if (memcmp(device1Mac, address, 6) == 0) {
       memcpy(LTK, device1LTK, 16);
       return 1;
-    }else if(memcmp(device2Mac, address, 6) == 0) {
+    } else if (memcmp(device2Mac, address, 6) == 0) {
       memcpy(LTK, device2LTK, 16);
       return 1;
     }
     return 0;
   });
-  BLE.setStoreIRK([](uint8_t* address, uint8_t* IRK){
+
+  BLE.setStoreIRK([](uint8_t *address, uint8_t *IRK) {
     Serial.print(F("New device with MAC : "));
-    btct.printBytes(address,6);
+    for (int i = 0; i < 6; i++) {
+      Serial.print(address[i], HEX);
+      if (i < 5)
+        Serial.print(":");
+    }
+    Serial.println();
     Serial.print(F("Need to store IRK   : "));
-    btct.printBytes(IRK,16);
-    return 1;
-  });
-  BLE.setStoreLTK([](uint8_t* address, uint8_t* LTK){
-    Serial.print(F("New device with MAC : "));
-    btct.printBytes(address,6);
-    Serial.print(F("Need to store LTK   : "));
-    btct.printBytes(LTK,16);
+    for (int i = 0; i < 16; i++) {
+      Serial.print(IRK[i], HEX);
+      if (i < 15)
+        Serial.print(":");
+    }
+    Serial.println();
     return 1;
   });
 
-  while(1){
+  BLE.setStoreLTK([](uint8_t *address, uint8_t *LTK) {
+    Serial.print(F("New device with MAC : "));
+    for (int i = 0; i < 6; i++) {
+      Serial.print(address[i], HEX);
+      if (i < 5)
+        Serial.print(":");
+    }
+    Serial.println();
+    Serial.print(F("Need to store LTK   : "));
+    for (int i = 0; i < 16; i++) {
+      Serial.print(LTK[i], HEX);
+      if (i < 15)
+        Serial.print(":");
+    }
+    Serial.println();
+    return 1;
+  });
+
+  while (1) {
     if (!BLE.begin()) {
       Serial.println("starting BLE failed!");
       delay(200);
@@ -124,17 +168,20 @@ void setup() {
     BLE.setAdvertisedService(testService);
     testService.addCharacteristic(stringCharacteristic);
     testService.addCharacteristic(secureValue);
+    testService.addCharacteristic(dataCharacteristic);
 
     BLE.addService(testService);
 
     stringCharacteristic.writeValue("string");
     secureValue.writeValue(0);
 
+    dataCharacteristic.setEventHandler(BLEWritten, onDataReceived);
+
     delay(1000);
 
     BLE.setPairable(false);
 
-    if(!BLE.advertise()){
+    if (!BLE.advertise()) {
       Serial.println("failed to advertise bluetooth.");
       BLE.stopAdvertise();
       delay(500);
@@ -151,12 +198,12 @@ void loop() {
   BLEDevice central = BLE.central();
 
   if (central && central.connected()) {
-    Serial.print("Connected to central: ");
-    Serial.println(central.address());
+    Serial.println("Connected to central");
 
-    long currentMillis = millis();
-    digitalWrite(CTRL_LED, secureValue.value() > 0 ? HIGH : LOW);
-  } else {
-    digitalWrite(CTRL_LED, LOW);
+    while (central.connected()) {
+      // wait for central to disconnect
+    }
+
+    Serial.println("Disconnected from central");
   }
 }
